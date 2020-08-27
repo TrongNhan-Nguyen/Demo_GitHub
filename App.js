d40354238057d7178dc24836e03e1afe40977496
oldman14 <thaibuaplol@gmail.com>
2020-08-28 00:56:43 +0700

    Thai Demo2

--- App.js
+++ App.js
@@ -2,11 +2,9 @@ import React from 'react';
 import {Text, View} from 'react-native';
 
 const App = () => {
-<<<<<<< HEAD
     return <View><Text>Hà Xuân Dương</Text>
-     <Text>Ngô Hoàng</Text>
+     <Text>Ngô Hoàng Thái</Text>
     </View>
-=======
   return (
     <View>
       <Text>Hà Xuân Dương - Dê</Text>
@@ -14,7 +12,6 @@ const App = () => {
       <Text>Nguyễn Trọng Nhân 1</Text>
     </View>
   );
->>>>>>> b90627ab4ff6f4d1bf8b7842f5bca28e182ce50c
 };
 
 export default App;
