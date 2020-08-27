a9e7bae70f550e60419fea9e7f18ab315473b637
Trong Nhan <trongnhannguyen94@gmail.com>
2020-08-27 23:34:46 +0700

    add comment in app.js

--- src/stack/Auth_Stack.js
+++ src/stack/Auth_Stack.js
@@ -3,13 +3,14 @@ import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Sign_in from '../screen/Sign_in';
+import { SIGN_IN } from '../screen/Screen_Name';
 const Stack = createStackNavigator();
 
 const Auth_Stack = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Sign_in} />
+        <Stack.Screen name={SIGN_IN} component={Sign_in} />
       </Stack.Navigator>
     </NavigationContainer>
   );
