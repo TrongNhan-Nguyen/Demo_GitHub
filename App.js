4e2e55742f335e8299451567fc7dc4bc86730b59
DaoDTQ-FD <trong.quang.dao@gmail.com>
2020-08-28 12:27:21 +0700

    Hello my name í Dao

--- App.js
+++ App.js
@@ -3,24 +3,9 @@ import {Text, View} from 'react-native';
 
 const App = () => {
     return <View><Text>Hà Xuân Dương</Text>
-     <Text>Ngô Hoàng Thái</Text>
+        <Text>Ngô Hoàng Thái</Text>
+        <Text>Đinh Trọng Quang Đạo</Text>
     </View>
-  return (
-    <View>
-      <Text>Hà Xuân Dương - Dê</Text>
-      <Text>Nguyễn Trọng Nhân 2</Text>
-      <Text>Nguyễn Trọng Nhân 1</Text>
-<<<<<<< HEAD
-      <Text>Nguyen Thanh Truc hahahahahahahah</Text>
-      <Text>Đinh Trọng Quang Đạo</Text>
-        <Text>Ha Xuan Duong demo2</Text>
-        <Text>Ha Xuan Duong demo3</Text>
-
-=======
-      <Text>Ngô Hoàng Thái</Text>
->>>>>>> 3589e926685cde0327025934aa78e5cd1bee7704
-    </View>
-  );
 };
 
 export default App;
